452dd562f5afc5950a7a8dba7c881d793ebf22d9
cameron nicklaus <camnicklaus@camerons-MacBook-Air.local>
2021-03-14 14:44:24 -0700

    wip

--- script.js
+++ script.js
@@ -1,31 +1,67 @@
-var username = prompt('what is your username')
+// var username = prompt('what is your username')
+window.requestAnimationFrame(draw);
 
-const canvas = document.querySelector('canvas')
+function draw() {
+    console.log(document.body)
+    document.body.addEventListener('keydown', handleCharacterMove)
 
-canvas.width = window.innerWidth
-canvas.height = window.innerHeight
+    function handleCharacterMove(event) {
+        // debugger
+        event.preventDefault();
 
-const ctx = canvas.getContext('2d')
-const numGrids = 30
-const gridWidth = Math.floor(window.innerWidth / numGrids)
-const halfGridWidth = gridWidth / 2
+        const { code } = event;
 
-function player({ centerX, centerY }) {
-    ctx.beginPath()
-    ctx.arc(centerX, centerY, halfGridWidth, 0, 2 * Math.PI, false)
-    ctx.fillStyle = 'purple'
-    ctx.fill()
+        switch(code) {
+            case "ArrowLeft":
+                console.log('called arrow left');
+                moveRight();
+                break;
+            case "ArrowRight":
+                console.log('called arrow right');
+                break;
+            case "ArrowUp":
+                console.log('called arrow up');
+                break;
+            case "ArrowDown":
+                console.log('called arrow down');
+                break;
+        }
+    }
+
+    const canvas = document.querySelector('canvas')
+
+    canvas.width = window.innerWidth
+    canvas.height = window.innerHeight
+
+    const ctx = canvas.getContext('2d')
+    const numGrids = 30
+    const gridWidth = Math.floor(window.innerWidth / numGrids)
+    const halfGridWidth = gridWidth / 2
+
+    function player({ centerX, centerY }) {
+        ctx.beginPath()
+        ctx.arc(centerX, centerY, halfGridWidth, 0, 2 * Math.PI, false)
+        ctx.fillStyle = 'purple'
+        ctx.fill()
+    }
+
+    function wall(x, y, height) {
+        ctx.beginPath()
+        ctx.strokeStyle = 'black'
+        ctx.fillStyle = 'white'
+        ctx.rect(x, y, gridWidth, height)
+        ctx.fill()
+        ctx.stroke()
+    }
+
+    wall(gridWidth * (numGrids / 2), 0, gridWidth * 6)
+    wall(gridWidth * (numGrids / 3), gridWidth * 4, gridWidth * 10)
+    wall(gridWidth * (numGrids - 8), gridWidth * 2, gridWidth * 8)
+
+    player({ centerX: halfGridWidth, centerY: window.innerHeight / 2 })
+    function moveRight() {
+        player()
+    }
+
+    window.requestAnimationFrame(draw);
 }
-
-function wall(x, y, height) {
-    ctx.beginPath()
-    ctx.strokeStyle = 'black'
-    ctx.fillStyle = 'white'
-    ctx.rect(x, y, gridWidth, height)
-    ctx.fill()
-    ctx.stroke()
-}
-
-wall(gridWidth * (numGrids / 2), gridWidth, gridWidth * 5)
-
-player({ centerX: halfGridWidth, centerY: window.innerHeight / 2 })
